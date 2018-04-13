1536acbc7513ce0d0592f93890ae430efee38858
Cabilangan112 <jassencabilangan@gmail.com>
2018-04-13 10:31:17 +0800

    calc

---
+++ jassen/calculator.py
@@ -0,0 +1,31 @@
+
+
+
+number_1 = int(input('Enter your first number: '))
+operation = raw_input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+number_2 = int(input('Enter your second number: '))
+
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
+
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
+
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
+
+elif operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
+
+else:
+    print('invalid operation.')
