0f84a31e457ebf691a2fa624436dffd7b5b7836b
Cabilangan112 <jassencabilangan@gmail.com>
2018-04-13 15:44:53 +0800

    gg calculator

--- jassen/Greet.py
+++ jassen/Greet.py
@@ -1,1 +1,2 @@
- 
+Enter_text = input("Enter Your Name: ")
+print ("What's up, {}".format(Enter_text))
--- jassen/max.py
+++ jassen/max.py
@@ -1,6 +1,6 @@
 a=[1,2,3,4,6]
 max= 0
 for i in a:
-        if i > max:
-            max=i
+	if i > max:
+		max=i
 print(max)
